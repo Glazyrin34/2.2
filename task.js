f0e29b20d96a16009b70088bf27344a67b0918fa
Ilya Glazyrin <glazyrinilya2011@gmail.com>
2021-03-09 23:48:47 +0500

    initial commit

---
+++ task.js
@@ -0,0 +1,30 @@
+String.prototype.isPalindrome = function isPalindrome() {
+    let string = this.toLowerCase().replace(/\s/g, '').split("");
+    if (string.join() === string.reverse().join()) {
+        return true;
+    } else { 
+        return false;
+    }
+}
+
+function getAverageMark(marks) {
+    let sum = 0;
+
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    
+    let averageMark = sum / marks.length;
+    let roundedAverage = Math.round(averageMark);
+
+    return roundedAverage;
+}
+
+function checkBirthday(birthday) {
+    // код для задачи №3 писать здесь
+    // return verdict
+}
